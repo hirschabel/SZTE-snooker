f9d7fdd4decb54c53fad2378bd3e7246431dc5a1
hirschabel <hirschabel@gmail.com>
2023-11-14 19:42:09 +0100

    Prezentálható állapot előkészítése

--- snooker/main.py
+++ snooker/main.py
@@ -5,14 +5,40 @@ from holes import find_holes
 
 if __name__ == '__main__':
     # Read the image
-    img = cv2.imread("asd2.png")
-    cv2.imshow('Original image', img)
+    orig_image = cv2.imread("videos/bemutato.png")
 
-    img, snooker_table = find_snooker_table(img)
+    # Find snooker table boundaries
+    img, snooker_table = find_snooker_table(orig_image.copy())
 
-    # Display the result
-    cv2.imshow('Snooker Table', img)
-    cv2.imshow('asd', find_balls(snooker_table))
-    cv2.imshow('Holes', find_holes(snooker_table))
+    # Find balls present on table
+    balls = find_balls(snooker_table.copy())
+
+    # Find holes
+    holes = find_holes(snooker_table.copy())
+
+    # Final image
+    final_image = cv2.addWeighted(balls, 0.5, holes, 0.5, 0)
+
+    # ======Results======
+    # Original Image
+    cv2.imshow('Original Image', orig_image)
     cv2.waitKey(0)
+
+    # Snooker Table
+    cv2.imshow('Snooker Table', snooker_table)
+    cv2.waitKey(0)
+
+    # Balls Highlighted
+    cv2.imshow('Balls Highlighted', balls)
+    cv2.waitKey(0)
+
+    # Holes Highlighted
+    cv2.imshow('Holes Highlighted', holes)
+    cv2.waitKey(0)
+
+    # Final image
+    cv2.imshow('Final', final_image)
+    cv2.waitKey(0)
+
+    # Clean-up
     cv2.destroyAllWindows()
